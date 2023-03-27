db753195911852999e3c23d373aa051ae58079ce
abhishek90894 <abhisheksrivastava90894@gmail.com>
2023-03-27 19:12:36 +0530

    final commit

--- src/main/java/com/validation/springBoot/validationPractice/controller/userController.java
+++ src/main/java/com/validation/springBoot/validationPractice/controller/userController.java
@@ -7,13 +7,10 @@ import jakarta.validation.Valid;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
-import java.util.Optional;
-import java.util.logging.Logger;
 
 @Slf4j
 @RestController
