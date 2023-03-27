09d620fea35f453899132d7464db53957621914b
abhishek90894 <abhisheksrivastava90894@gmail.com>
2023-03-27 13:07:58 +0530

    application.properties updated

--- src/main/java/com/validation/springBoot/validationPractice/ValidationPracticeApplication.java
+++ src/main/java/com/validation/springBoot/validationPractice/ValidationPracticeApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class ValidationPracticeApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(ValidationPracticeApplication.class, args);
 	}
 
